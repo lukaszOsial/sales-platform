92d03865a4606f91f1c230b408c297b5dd05a8e2
Łukasz Osial <lukasz.osial@gmail.com>
2023-02-05 09:56:08 +0100

    create a project

---
+++ sales-platform_spring-boot/src/main/java/com/lukaszOsial/salesplatform/SalesPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.lukaszOsial.salesplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesPlatformApplication.class, args);
+	}
+
+}
---
+++ sales-platform_spring-boot/src/test/java/com/lukaszOsial/salesplatform/SalesPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukaszOsial.salesplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
