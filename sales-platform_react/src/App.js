3612457121ea9b94471496f13a01fb775a758d91
Łukasz Osial <lukasz.osial@gmail.com>
2023-02-10 11:02:27 +0100

    installing react-router-dom and creating routing for contractors

--- sales-platform_react/src/App.js
+++ sales-platform_react/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ListContractorComponent from "./components/contractor/ListContractorComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
@@ -6,11 +7,16 @@ import FooterComponent from "./components/FooterComponent";
 function App() {
 	return (
 		<div>
-			<HeaderComponent />
-			<div className='container'>
-				<ListContractorComponent />
-			</div>
-			<FooterComponent/>
+			<Router>
+				<HeaderComponent />
+				<div className='container'>
+					<Routes>
+						<Route exact path ='/' element={<ListContractorComponent/>}/>
+						<Route exact path ='/contractors' element={<ListContractorComponent/>}/>
+					</Routes>
+				</div>
+				<FooterComponent />
+			</Router>
 		</div>
 	);
 }
