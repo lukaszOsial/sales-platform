c62ce5f08ad21c747b121a52b5ec78fbad51e37d
Łukasz Osial <lukasz.osial@gmail.com>
2023-02-24 14:57:06 +0100

    commit

--- sales-platform_spring-boot/src/main/java/com/lukaszOsial/salesplatform/contractor/controller/ProductController.java
+++ sales-platform_spring-boot/src/main/java/com/lukaszOsial/salesplatform/contractor/controller/ProductController.java
@@ -1,9 +1,11 @@
 package com.lukaszOsial.salesplatform.contractor.controller;
 
+import com.lukaszOsial.salesplatform.contractor.exception.ResourceNotFoundException;
 import com.lukaszOsial.salesplatform.contractor.model.Contractor;
 import com.lukaszOsial.salesplatform.contractor.model.Product;
 import com.lukaszOsial.salesplatform.contractor.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,7 +19,7 @@ public class ProductController {
 
     //get all products method
     @GetMapping("/products")
-    public List<Product> getAllContractors() {
+    public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
@@ -25,5 +27,30 @@ public class ProductController {
     @PostMapping("/products")
     public Product createProduct(@RequestBody Product product) {
         return productRepository.save(product);
+    }
+
+    //get product by id
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Nie istnieje towar o numerze identyfikacyjnym: " + id));
+        return ResponseEntity.ok(product);
+    }
+
+    //update product method
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Nie istnieje produkt o numerze identyfikacyjnym: " + id));
+        product.setCode(productDetails.getCode());
+        product.setName(productDetails.getName());
+        product.setType(productDetails.getType());
+        product.setGrossPrice(productDetails.getGrossPrice());
+        product.setNetPrice(productDetails.getNetPrice());
+        product.setVatRate(productDetails.getVatRate());
+        product.setQuantity(productDetails.getQuantity());
+
+        Product updatedProduct = productRepository.save(product);
+        return ResponseEntity.ok(updatedProduct);
     }
 }
